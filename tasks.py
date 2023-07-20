a8d56f96f89ae75fd81eff157ccef8b1267b8cd2
Zihang Tang <eden@Zihangs-MacBook-Pro.local>
2023-07-20 15:47:53 +0800

    feat: support modified userophash

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 import os
 import math
 import requests
-import logging
+from web3 import Web3
 import json
 from celery import Celery
 from database import Database
@@ -9,6 +9,7 @@ from op_model import get_chainid
 from encode import get_user_op_hash
 from celery.signals import task_prerun
 from celery.utils.log import get_task_logger
+import binascii
 
 app = Celery('schedule', broker=os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0'))
 db = Database()
@@ -21,25 +22,68 @@ app.conf.update({
 app.log.setup_task_loggers()
 logger = get_task_logger(__name__)
 
+def int_to_bytes(i):
+    return i.to_bytes((i.bit_length() + 7) // 8, byteorder='big')
+
+def bytes_to_hex(b, include_0x=True):
+    hex_str = binascii.hexlify(b).decode()
+    if include_0x:
+        hex_str = '0x' + hex_str
+    return hex_str
+
+
 @task_prerun.connect
 def task_prerun_handler(sender=None, task_id=None, task=None, args=None, kwargs=None, **kwds):
     logger.info(f'Task {task_id} is about to run with args: {args} and kwargs: {kwargs}')
 
 def fetch_gas_prices(chain_id):
     logger.info('Fetching gas prices for chain id: %s', chain_id)
-    url = f"https://gas-api.metaswap.codefi.network/networks/{chain_id}/suggestedGasFees"
-    response = requests.get(url)
-    data = response.json()
 
-    logger.info(f"here gas response {data}")
+    if chain_id == 534353:
+        url = 'https://scroll-alphanet.blastapi.io/30947b96-0c7b-4ff4-b48c-40925343ff7f'
+        headers = {'Content-Type': 'application/json'}
+        data = {
+            "jsonrpc": "2.0",
+            "id": 73,
+            "method": "eth_gasPrice",
+            "params": []
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        logger.info(f"here gas response {response.json()}")
+        result = response.json()['result']
+        value = int(result, 16)  # Converting hexadecimal to integer
+        suggested_max_fee_per_gas_mwei = math.ceil(value / 1000000.0) * 10
+        suggested_max_priority_fee_per_gas_mwei = suggested_max_fee_per_gas_mwei
 
-    suggested_max_fee_per_gas = hex(math.ceil(float(data["high"]["suggestedMaxFeePerGas"]) * 1000))
-    suggested_max_priority_fee_per_gas = hex(math.ceil(float(data["high"]["suggestedMaxPriorityFeePerGas"]) * 1000))
+    else:
+        url = f"https://gas-api.metaswap.codefi.network/networks/{chain_id}/suggestedGasFees"
+        response = requests.get(url)
+        data = response.json()
+
+        logger.info(f"here gas response {data}")
+
+        suggested_max_fee_per_gas_mwei = math.ceil(float(data["high"]["suggestedMaxFeePerGas"]) * 1000)
+        suggested_max_priority_fee_per_gas_mwei = math.ceil(float(data["high"]["suggestedMaxPriorityFeePerGas"]) * 1000)
+
+    # 将mwei转换为wei
+    suggested_max_fee_per_gas_wei = Web3.to_wei(suggested_max_fee_per_gas_mwei, 'mwei')
+    suggested_max_priority_fee_per_gas_wei = Web3.to_wei(suggested_max_priority_fee_per_gas_mwei, 'mwei')
+
+    # 调整 suggested_max_fee_per_gas_wei 的值
+    suggested_max_fee_per_gas_wei = min(suggested_max_fee_per_gas_wei, Web3.to_wei(200, 'gwei'))
+    suggested_max_fee_per_gas_wei = int(suggested_max_fee_per_gas_wei * 1.2)
+
+    # 如果 chain_id 等于 534353，将 suggested_max_priority_fee_per_gas_wei 的值设置为 suggested_max_fee_per_gas_wei 的值
+    if chain_id == 534353:
+        suggested_max_priority_fee_per_gas_wei = suggested_max_fee_per_gas_wei
+
+    suggested_max_fee_per_gas_wei = hex(suggested_max_fee_per_gas_wei)
+    suggested_max_priority_fee_per_gas_wei = hex(suggested_max_priority_fee_per_gas_wei)
 
     logger.info('Fetched and converted gas prices: MaxFeePerGas = %s, MaxPriorityFeePerGas = %s',
-                suggested_max_fee_per_gas, suggested_max_priority_fee_per_gas)
+                suggested_max_fee_per_gas_wei, suggested_max_priority_fee_per_gas_wei)
 
-    return suggested_max_fee_per_gas, suggested_max_priority_fee_per_gas
+    return suggested_max_fee_per_gas_wei, suggested_max_priority_fee_per_gas_wei
 
 def update_gas_prices_in_userop(data, suggested_max_fee_per_gas, suggested_max_priority_fee_per_gas, chainid):
     logger.info('Updating gas prices in user operation')
